ffb29e8831c52b4378edfdb05fcf89b37d33ab5c
Jeffri Sandy <jeffri.sandy@gmail.com>
2019-01-31 13:13:49 +0700

    ready for submission

--- app/src/main/java/com/example/android/soccerscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/soccerscorekeeper/MainActivity.java
@@ -15,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
     int cornerA = 0;
     int foulA = 0;
 
-    int teamBScore = 0;
+    int goalB = 0;
+    int shotOffGoalB = 0;
+    int shotOnGoalB = 0;
+    int freekickB = 0;
+    int cornerB = 0;
+    int foulB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+
+    /*********
+     * TEAM A
+     *********
+     */
 
     /**
      * TEAM A GOAL, increase by 1
@@ -115,25 +126,120 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /**
-     * Displays the given score for Team B.
+    /*********
+     * TEAM B
+     *********
      */
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+
+    /**
+     * TEAM B GOAL, increase by 1
+     */
+    public void addGoalB(View v) {
+
+        goalB += 1;
+        displayForTeamB(goalB, "goal");
+
     }
+
+    /**
+     * TEAM B Shot OFF Goal, increase by 1
+     */
+    public void addShotOffGoalB(View v) {
+
+        shotOffGoalB += 1;
+        displayForTeamB(shotOffGoalB, "shot_off_goal");
+
+    }
+
+    /**
+     * TEAM B Shot ON Goal, increase by 1
+     */
+    public void addShotOnGoalB(View v) {
+
+        shotOnGoalB += 1;
+        displayForTeamB(shotOnGoalB, "shot_on_goal");
+
+    }
+
+    /**
+     * TEAM B Freekick, increase by 1
+     */
+    public void addFreekickB(View v) {
+
+        freekickB += 1;
+        displayForTeamB(freekickB, "freekick");
+
+    }
+
+    /**
+     * TEAM B Corner, increase by 1
+     */
+    public void addCornerB(View v) {
+
+        cornerB += 1;
+        displayForTeamB(cornerB, "corner");
+
+    }
+
+    /**
+     * TEAM B Foul, increase by 1
+     */
+    public void addFoulB(View v) {
+
+        foulB += 1;
+        displayForTeamB(foulB, "foul");
+
+    }
+
+    /**
+     * Displays the given GOAL for Team B.
+     */
+    public void displayForTeamB(int score, String name) {
+
+
+        if (name == "goal") {
+            TextView scoreView = (TextView) findViewById(R.id.team_b_goal);
+            scoreView.setText(String.valueOf(score));
+        } else if (name == "shot_off_goal") {
+            TextView scoreView = (TextView) findViewById(R.id.shot_off_goal_b);
+            scoreView.setText(String.valueOf(score));
+        } else if (name == "shot_on_goal") {
+            TextView scoreView = (TextView) findViewById(R.id.shot_on_goal_b);
+            scoreView.setText(String.valueOf(score));
+        } else if (name == "freekick") {
+            TextView scoreView = (TextView) findViewById(R.id.freekick_b);
+            scoreView.setText(String.valueOf(score));
+        } else if (name == "corner") {
+            TextView scoreView = (TextView) findViewById(R.id.corner_b);
+            scoreView.setText(String.valueOf(score));
+        } else if (name == "foul") {
+            TextView scoreView = (TextView) findViewById(R.id.foul_b);
+            scoreView.setText(String.valueOf(score));
+        }
+
+
+    }
+
+
 
 
     /**
      * reset both team A & B score to zerp.
      */
     public void reset(View v) {
-        int goalA = 0;
-        int shotOffGoalA = 0;
-        int shotOnGoalA = 0;
-        int freekickA = 0;
-        int cornerA = 0;
-        int foulA = 0;
+        goalA = 0;
+        shotOffGoalA = 0;
+        shotOnGoalA = 0;
+        freekickA = 0;
+        cornerA = 0;
+        foulA = 0;
+
+        goalB = 0;
+        shotOffGoalB = 0;
+        shotOnGoalB = 0;
+        freekickB = 0;
+        cornerB = 0;
+        foulB = 0;
 
         displayForTeamA(goalA, "goal");
         displayForTeamA(shotOffGoalA, "shot_off_goal");
@@ -141,6 +247,14 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(freekickA, "freekick");
         displayForTeamA(cornerA, "corner");
         displayForTeamA(foulA, "foul");
+
+        displayForTeamB(goalB, "goal");
+        displayForTeamB(shotOffGoalB, "shot_off_goal");
+        displayForTeamB(shotOnGoalB, "shot_on_goal");
+        displayForTeamB(freekickB, "freekick");
+        displayForTeamB(cornerB, "corner");
+        displayForTeamB(foulB, "foul");
+
 
     }
 }
